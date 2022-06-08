53add6501c8dd469d546193c67366b433f13efbd
MarcoBorgess <marco.borges@branef.com.br>
2022-06-07 23:29:18 -0300

    mudanças

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 const TOKEN = 'OTgzODgyMTg1NDM2MTc2NDM0.Go5lA1.6vDvWnqfltCKZepbmzd7mlHUtedF4FjAgFJf5c';
 var prefix = "$";
+var baralho = "2ylp40zxgtcc";
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -26,20 +27,33 @@ client.on('message', async message => {
         })
     }
 
-    if (command === "b" || command === "baralho" || command === "deck" || command === "d") {
+    if (command === "baralhoNovo") {
         const data = axios({
             url: 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1',
             headers: {'accept': 'application/json'}
         }).then((result) => {
             message.channel.send(`Código do seu baralho: \n\n${result.data.deck_id}`);
+            baralho = result.data.deck_id;
         }).catch((error) => {
             console.log(error);
         })
     }
 
+    if (command === "r" || command === "reset") {
+        const data = axios({
+            url: `https://deckofcardsapi.com/api/deck/${baralho}/shuffle/`,
+            headers: {'accept': 'application/json'}
+        }).then((result) => {
+            message.channel.send(`Baralho RESETADO\nCódigo do baralho: \n\n${result.data.deck_id}`);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
+
     if (command === "p" || command === "poker") {
         const data = axios({
-            url: `https://deckofcardsapi.com/api/deck/${args}/draw/?count=1`,
+            url: `https://deckofcardsapi.com/api/deck/${baralho}/draw/?count=1`,
             headers: {'accept': 'application/json'}
         }).then((result) => {
             const embed = new Discord.MessageEmbed()
